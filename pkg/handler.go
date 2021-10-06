be20430b8e3eb710584dc090a83b05bd4a40929f
Andreas Tihor <andreastihor@Andreass-MacBook-Pro.local>
2021-10-06 12:25:19 +0700

    feat: adding handler to the folder structure

---
+++ pkg/handler.go
@@ -0,0 +1,7 @@
+package pkg
+
+import "github.com/gofiber/fiber/v2"
+
+func HandlerGetAllBook(ctx *fiber.Ctx) string {
+	return "all books"
+}
--- routes/route.go
+++ routes/route.go
@@ -1,6 +1,9 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	handler "github.com/andreasaiforesee/template/pkg"
+	"github.com/gofiber/fiber/v2"
+)
 
 func SetupRoutes(app *fiber.App) {
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -8,7 +11,8 @@ func SetupRoutes(app *fiber.App) {
 	})
 
 	app.Get("/books", func(c *fiber.Ctx) error {
-		return c.SendString("all books")
+		res := handler.HandlerGetAllBook(c)
+		return c.SendString(res)
 	})
 
 	app.Get("/book", func(c *fiber.Ctx) error {
